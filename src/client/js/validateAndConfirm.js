4b0b1c32d1cf7f2ff9fd84cc6a22fbddf5d83cf4
Ahmed Asiri <ia7mad13@gmail.com>
2020-08-03 16:27:39 +0300

    adding sweetalert2 files

--- src/client/js/validateAndConfirm.js
+++ src/client/js/validateAndConfirm.js
@@ -3,6 +3,9 @@ import {saveTrip} from "./trips";
 import {cardPreviewGenerator} from "./trips";
 import {validate} from "./validate";
 import {isCurrent} from "./current";
+import Swal from 'sweetalert2';
+import 'sweetalert2/src/sweetalert2.scss';
+
 
 export async function submitData(eve) {
     // this function will get data from the server then validate it then ask the user for confirm or cancel
@@ -31,10 +34,26 @@ export async function submitData(eve) {
 
 
 
-function previewAndConfirm(cardData) {
+async function previewAndConfirm(cardData) {
     // preview the Trip Card to the user, he can confirm or cancel it.
     let cardPreviewElement = cardPreviewGenerator(cardData);
-    saveTrip(cardData);
+
+    Swal.fire({
+        title: 'Trip Preview',
+        html: cardPreviewElement,
+        showCancelButton: true,
+        allowOutsideClick: false,
+        cancelButtonText: "Cancel Trip",
+        confirmButtonText: "Save Trip",
+        cancelButtonColor: "#d33",
+        confirmButtonColor: "#3085d6"
+      })
+    .then((dismiss) => {
+        if(dismiss.isConfirmed){
+            //confirm the trip
+            saveTrip(cardData);
+        }
+    });
 }
 
 async function gettingDataAndLoading(tripData) {
