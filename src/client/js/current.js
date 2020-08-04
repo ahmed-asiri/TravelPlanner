8b3d263506df6a9cd53da3709d71c208a6dea75d
Ahmed Asiri <ia7mad13@gmail.com>
2020-08-04 05:26:23 +0300

    remove the current endpoint from the server 3

--- src/client/js/current.js
+++ src/client/js/current.js
@@ -1,7 +1,13 @@
 export function isCurrent(depart) {
     // help us to decide to use the current weather or forecast
-    if(((depart.getTime() - new Date().getTime()) / (1000*60*60*24)) <= 7){
-        return "true";
-    }
-    return "false";
+
+    
+//    if(((depart.getTime() - new Date().getTime()) / (1000*60*60*24)) <= 7){
+//        return "true";
+//    }
+
+    if(new Date().getDate() - depart.getDate() < 7 &&  new Date().getMonth() === depart.getMonth() && new Date().getFullYear() === depart.getFullYear())
+        return true;
+    
+    return false;
 }
