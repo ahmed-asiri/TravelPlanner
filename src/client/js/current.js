d09bbf4a8de799fd0c163994ddf4e48145e154e4
Ahmed Asiri <ia7mad13@gmail.com>
2020-08-04 04:05:44 +0300

    fixing current.js isCurrent() for date

--- src/client/js/current.js
+++ src/client/js/current.js
@@ -1,6 +1,6 @@
 export function isCurrent(data) {
     // help us to decide to use the current weather or forecast
-    if(((data.arrive.valueAsDate.getTime() - new Date().getTime()) / (1000*60*60*24)) <= 7){
+    if(((data.arrive.getTime() - new Date().getTime()) / (1000*60*60*24)) <= 7){
         return true;
     }
     return false;
