6d9788383c43022ebdfb71190b2c8e151c36e710
Ahmed Asiri <ia7mad13@gmail.com>
2020-08-05 07:36:32 +0300

    adding postcss-loader to the package,json file

--- src/client/js/current.js
+++ src/client/js/current.js
@@ -1,11 +1,5 @@
 export function isCurrent(depart) {
     // help us to decide to use the current weather or forecast
-
-    
-//    if(((depart.getTime() - new Date().getTime()) / (1000*60*60*24)) <= 7){
-//        return "true";
-//    }
-
     if(new Date().getDate() - depart.getDate() < 7 &&  new Date().getMonth() === depart.getMonth() && new Date().getFullYear() === depart.getFullYear())
         return true;
     
